5536f74a9e639dccbf14c3e97fbd8f37f803fb8f
Mithul-varshan <mithul0605@gmail.com>
2026-01-03 12:09:29 +0530

    removed unessary files in backend

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const db = require('./db');
-
-const authRoutes = require('./routes/authRoutes');
-const tripRoutes = require('./routes/tripRoutes');
-
 dotenv.config();
 
 const app = express();
@@ -15,25 +11,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/trips', tripRoutes);
-
-// Basic Route
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
-// Test DB Route
-app.get('/test-db', async (req, res) => {
-  try {
-    const [rows] = await db.query('SELECT 1 + 1 AS solution');
-    res.json({ message: 'Database connected successfully', result: rows[0].solution });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Database connection failed', error: error.message });
-  }
-});
 
 // Start Server
 app.listen(PORT, () => {
